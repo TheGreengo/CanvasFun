fe7b89a572e757827b853363c6812efcba65c5b2
Braden <bradenoptimus@gmail.com>
2024-02-15 19:58:54 -0700

    huh

--- block.js
+++ block.js
@@ -1,9 +1,27 @@
-var c = document.getElementById("block-canvas");
-var ctx = c.getContext("2d");
+let c = document.getElementById("block-canvas");
+let ctx = c.getContext("2d");
+let het = 160
+let wid = 100
+let px 
 
 function update() {
-    for (i = 0; i < 100; i += 5) {
-        ctx.fillStyle = "rgba("+0+","+100+","+10+","+(255/255)+")";
-        ctx.fillRect( i, i, 10, 10 );
+    let g = 0;
+    let b = 0;
+    for (i = 0; i < het; i++) {
+        for (j = 0; j < wid; j++) {
+            if (((i+j) % 2) == 0) {
+                g = 255;
+                b = 0;
+            } else {
+                g = 0;
+                b = 255;
+            }
+            ctx.fillStyle = "rgba("+0+","+g+","+b+","+(255/255)+")";
+            ctx.fillRect( i*5, j*5, 5, 5);
+        }
     }
+}
+
+function clar() {
+    ctx.clearRect(0, 0, c.width, c.height);
 }
